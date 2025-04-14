8750d1ca76c0634b44a7171ed5cea5c475afb698
DESKTOP-25D2AVM\eddya <andres.piscog@gmail.com>
2025-04-14 09:21:36 +0200

    Creation of vuelax app

---
+++ vuelax-backend/src/main/java/com/example/vuelax/vuelax/VuelaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuelax.vuelax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelaxApplication.class, args);
+	}
+
+}
---
+++ vuelax-backend/src/test/java/com/example/vuelax/vuelax/VuelaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuelax.vuelax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
